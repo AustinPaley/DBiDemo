353d7ad11b1b2099db405eb66e14ea5d8189926a
Austin Paley <apaley@demandbase.com>
2020-05-22 13:52:48 -0400

    Added Enterprise Filtering

--- popup.js
+++ popup.js
@@ -46,8 +46,8 @@ prebuilt.addEventListener("change", (e) => {
   if (e.target.value === "Midmarket Audiences"){
     document.querySelector('#audience_one_name').value = "All Prospect Accounts"
     document.querySelector('#audience_one_accounts').value = "2500"
-    document.querySelector('#audience_one_visited').value = "849"
-    document.querySelector('#audience_one_engaged').value = "227"
+    document.querySelector('#audience_one_visited').value = "850"
+    document.querySelector('#audience_one_engaged').value = "223"
     document.querySelector('#audience_one_total').value = "1200"
 
     document.querySelector('#audience_two_name').value = "Awareness Audience"
@@ -69,16 +69,16 @@ prebuilt.addEventListener("change", (e) => {
     document.querySelector('#audience_four_total').value = "340"
 
     document.querySelector('#audience_five_name').value = "Customer Accounts"
-    document.querySelector('#audience_five_accounts').value = "40"
-    document.querySelector('#audience_five_visited').value = "36"
-    document.querySelector('#audience_five_engaged').value = "34"
-    document.querySelector('#audience_five_total').value = "300"
+    document.querySelector('#audience_five_accounts').value = "389"
+    document.querySelector('#audience_five_visited').value = "109"
+    document.querySelector('#audience_five_engaged').value = "75"
+    document.querySelector('#audience_five_total').value = "525"
 
 
     document.querySelector("#audience_name_change").value = "All Prospect Accounts"
     document.querySelector("#main_audience_accounts").value = "2500"
-    document.querySelector("#main_audience_visited").value = "849"
-    document.querySelector("#main_audience_engaged").value = "227"
+    document.querySelector("#main_audience_visited").value = "850"
+    document.querySelector("#main_audience_engaged").value = "223"
     document.querySelector("#main_audience_pageviews").value = "1200"
     document.querySelector("#main_audience_converted").value = "118"
     document.querySelector("#main_audience_opportunities").value = "117"
@@ -87,5 +87,51 @@ prebuilt.addEventListener("change", (e) => {
     document.querySelector("#main_audience_closerate").value = "12"
     document.querySelector("#main_audience_pipeline").value = "117000"
     document.querySelector("#main_audience_dealsize").value = "10000"
+  }
+
+  if (e.target.value === "Enterprise Audiences"){
+    document.querySelector('#audience_one_name').value = "All Prospect Accounts"
+    document.querySelector('#audience_one_accounts').value = "5000"
+    document.querySelector('#audience_one_visited').value = "2488"
+    document.querySelector('#audience_one_engaged').value = "557"
+    document.querySelector('#audience_one_total').value = "4000"
+
+    document.querySelector('#audience_two_name').value = "Awareness Audience"
+    document.querySelector('#audience_two_accounts').value = "4042"
+    document.querySelector('#audience_two_visited').value = "1013"
+    document.querySelector('#audience_two_engaged').value = "0"
+    document.querySelector('#audience_two_total').value = "1013"
+
+    document.querySelector('#audience_three_name').value = "Engagement Audience"
+    document.querySelector('#audience_three_accounts').value = "1253"
+    document.querySelector('#audience_three_visited').value = "1250"
+    document.querySelector('#audience_three_engaged').value = "375"
+    document.querySelector('#audience_three_total').value = "1400"
+
+    document.querySelector('#audience_four_name').value = "Pipeline Audience"
+    document.querySelector('#audience_four_accounts').value = "256"
+    document.querySelector('#audience_four_visited').value = "225"
+    document.querySelector('#audience_four_engaged').value = "202"
+    document.querySelector('#audience_four_total').value = "900"
+
+    document.querySelector('#audience_five_name').value = "Customer Accounts"
+    document.querySelector('#audience_five_accounts').value = "983"
+    document.querySelector('#audience_five_visited').value = "302"
+    document.querySelector('#audience_five_engaged').value = "298"
+    document.querySelector('#audience_five_total').value = "900"
+
+
+    document.querySelector("#audience_name_change").value = "All Prospect Accounts"
+    document.querySelector("#main_audience_accounts").value = "5000"
+    document.querySelector("#main_audience_visited").value = "2488"
+    document.querySelector("#main_audience_engaged").value = "577"
+    document.querySelector("#main_audience_pageviews").value = "4000"
+    document.querySelector("#main_audience_converted").value = "185"
+    document.querySelector("#main_audience_opportunities").value = "124"
+    document.querySelector("#main_audience_accountswon").value = "37"
+    document.querySelector("#main_audience_totalwon").value = "370000"
+    document.querySelector("#main_audience_closerate").value = "30"
+    document.querySelector("#main_audience_pipeline").value = "12400000"
+    document.querySelector("#main_audience_dealsize").value = "100000"
   }
 })
